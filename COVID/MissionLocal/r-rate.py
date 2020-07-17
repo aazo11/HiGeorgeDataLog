550327f991a37614bffa4f1ddd4d6b5cdc689812
Aaron Opell <aaron.opell@live.com>
2020-07-16 18:13:44 -0700

    SF R

---
+++ COVID/MissionLocal/r-rate.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import requests as rq
+import pytz
+
+def process_data(r):
+    final_r = r[r['Region'] == 'San Francisco']
+    final_r = final_r[final_r['Date'] > '2020-03-11']
+    final_r['one'] = 1
+    return final_r
+
+def get_updated_data(df):
+    return None
